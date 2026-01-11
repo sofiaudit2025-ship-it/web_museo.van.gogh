8a794554cb9209fb436df5d39680d98b4701de27
eemmalb <emma.lago@alumnos.udit.es>
2026-01-11 13:05:58 +0100

    texto onhover slideshow home

--- js/home.js
+++ js/home.js
@@ -9,7 +9,7 @@ document.addEventListener('mousemove', (e) => {
 
 items.forEach(item => {
   item.addEventListener('mouseenter', () => {
-    console.log("Ratón entró en:", item.className); // Mira la consola (F12)
+    console.log("Ratón entró en:", item.className); 
     
     let url = "";
     if (item.classList.contains('item-galeria')) url = "../css/media/img/noche_estrellada_cursor.png";
@@ -24,3 +24,51 @@ items.forEach(item => {
     mouseImg.style.display = 'none';
   });
 });
+
+
+
+// CARRUSEL 
+const imgs = document.querySelectorAll('.track img');
+const captions = document.querySelectorAll('.track .track-caption');
+let index = 1;
+function update() {
+    imgs.forEach(img => img.className = '');
+    captions.forEach(caption => caption.className = 'track-caption');
+
+    imgs[index].classList.add('active');
+    if (imgs[index - 1]) imgs[index - 1].classList.add('prev');
+    if (imgs[index + 1]) imgs[index + 1].classList.add('next');
+
+    captions[index].classList.add('active');
+    if (captions[index - 1]) captions[index - 1].classList.add('prev');
+    if (captions[index + 1]) captions[index + 1].classList.add('next');
+}
+
+document.querySelector('.gallery-arrow.right').onclick = () => {
+    index = (index + 1) % imgs.length;
+    update();
+};
+
+document.querySelector('.gallery-arrow.left').onclick = () => {
+    index = (index - 1 + imgs.length) % imgs.length;
+    update();
+};
+
+update();
+
+
+
+//  DESPLEGAR DATOS CURIOSOS
+function toggleFaq(el) {
+const answer = el.nextElementSibling;
+const arrow = el.querySelector(".arrow");
+
+if (answer.style.maxHeight) {
+    answer.style.maxHeight = null;
+    arrow.style.transform = "rotate(0deg)";
+} else {
+    answer.style.maxHeight = answer.scrollHeight + "px";
+    arrow.style.transform = "rotate(90deg)";
+}
+}
+
